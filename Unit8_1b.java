20665145c69dc7c6e800c746c5191315e5003e2b
Joon Kyo Kyo <>
2021-04-27 23:18:29 +0000

    Confirmed code function

--- Unit8_1b.java
+++ Unit8_1b.java
@@ -1,8 +1,15 @@
-public class Unit8_1a
+public class Unit8_1b
 {
   public int [] [] modifyRows (int [] [] array)
  { 
-return array;
+   for(int i = 0; i < array.length; i++){
+     if(i % 2 == 1){
+       for(int j = 0; j < array[i].length; j++){
+          array[i][j] = array[i - 1][j];
+       }
+     }
+    }
+    return array;
       }
     }
  
